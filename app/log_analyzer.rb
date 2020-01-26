e6c7a9321e5eb335c2c711a1a2dc2943f74d0a20
Dariusz Musielak <dariusz.musielak@netguru.pl>
2020-01-26 22:22:27 +0100

    Add first version of a solution

--- app/log_analyzer.rb
+++ app/log_analyzer.rb
@@ -5,8 +5,55 @@ class LogAnalyzer
 
   def initialize(file_path, analyze_type)
     @file_path = file_path
+    @data = []
     @analyze_type = analyze_type
   end
+
   def call
+    load_data
+    group_entries!
+    sort_and_print
+  end
+
+  private
+
+  def sort_and_print
+    results = {
+      visits: sort_by_views,
+      uniq_visits: sort_by_uniq_views
+    }.fetch(analyze_type)
+
+    print_results(results)
+  end
+
+  def load_data
+    File.open(file_path, 'r') do |file|
+      file.each_line do |line|
+        domain, ip = line.split(' ')
+        @data << { domain: domain, ip: ip }
+      end
+    end
+  end
+
+  def group_entries!
+    @data = data.group_by { |log_entry| log_entry[:domain] }
+  end
+
+  def sort_by_views
+    data.map do |domain, ip|
+      { domain: domain, count: ip.count }
+    end.sort_by(&:values).reverse
+  end
+
+  def sort_by_uniq_views
+    data.map do |domain, ip|
+      { domain: domain, count: ip.uniq.count }
+    end.sort_by(&:values).reverse
+  end
+
+  def print_results(results)
+    description = (analyze_type == :visits ? 'visits' : 'unique visits')
+
+    results.each { |result| puts "#{result[:domain]} - #{result[:count]} #{description}" }
   end
 end
