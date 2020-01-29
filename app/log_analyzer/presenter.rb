cb903c1a4b2980ab3bdb70553df0ec2ca19ee1ab
Dariusz Musielak <dariusz.musielak@netguru.pl>
2020-01-30 00:17:29 +0100

    Simplify presenter by using dictionary

--- app/log_analyzer/presenter.rb
+++ app/log_analyzer/presenter.rb
@@ -3,20 +3,11 @@
 module LogAnalyzer
   class Presenter
     def call(results, analyze_type)
-      description = fetch_description(analyze_type)
+      description = LogAnalyzer.i18n(analyze_type)
 
       results.each do |result|
         puts "#{result[:domain]} - #{result[:count]} #{description}"
       end
-    end
-
-    private
-
-    def fetch_description(analyze_type)
-      {
-        visits: 'visits',
-        uniq_visits: 'unique visits'
-      }.fetch(analyze_type)
     end
   end
 end
